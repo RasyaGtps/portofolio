2b889298d2d4feb8b09b9a5b1e1f19db42acffed
RasyaGtps <topengkayu07@gmail.com>
2025-06-29 14:19:19 +0700

    fixing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,17 @@
 module.exports = {
     siteUrl: 'https://www.rasyarayhandev.my.id', // Ganti ini dengan URL aslimu
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    },
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 7000,
+    exclude: ['/api/*'],
   }
   
